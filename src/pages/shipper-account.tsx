6cfb37395a029400ef8f09578731add29bf27039
Niphawan Phoopha <phoopha.miw@gmail.com-at-518316016567>
2020-12-24 19:42:00 +0700

    [fix] file name

--- src/pages/shipper-account.tsx
+++ src/pages/shipper-account.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ShipperAccount from '../components/shipper/shipper-account/ShipperAccount';
+import ShipperAccount from '../components/shipper/shipper-account/shipper-account';
 
 const Jobs = () => {
   return <ShipperAccount />;
