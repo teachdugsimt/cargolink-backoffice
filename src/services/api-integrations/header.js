09acac3dd69a57181b02dead77af7a1b6e29a80b
Niphawan Phoopha <phoopha.miw@gmail.com-at-518316016567>
2021-02-01 18:32:00 +0700

    [fix] delete token when logout

--- src/services/api-integrations/header.js
+++ src/services/api-integrations/header.js
@@ -2,7 +2,8 @@ const Header = async (is_login = null, is_upload = null, timeout = 20000, requir
   let baseURL = process.env.API_ENDPOINT;
   let header = {};
   let token = await JSON.parse(localStorage.getItem('profileLocal'));
-  const language = await localStorage.getItem('profileLanguage');
+  let language = await localStorage.getItem('profileLanguage');
+  if (!language) language = 'th';
 
   if (!token) {
     token = {
@@ -26,7 +27,7 @@ const Header = async (is_login = null, is_upload = null, timeout = 20000, requir
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        'Accept-Language': language ? language : 'th',
+        'Accept-Language': language,
       },
       timeout: 20000,
     };
@@ -37,7 +38,7 @@ const Header = async (is_login = null, is_upload = null, timeout = 20000, requir
         'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
         Accept: '*/*',
         Authorization: 'Bearer ' + token.idToken,
-        'Accept-Language': language ? language : 'th',
+        'Accept-Language': language,
       },
       timeout: timeout,
     };
@@ -48,7 +49,7 @@ const Header = async (is_login = null, is_upload = null, timeout = 20000, requir
         'Content-Type': 'application/json',
         Accept: 'application/json',
         Authorization: 'Bearer ' + token.idToken,
-        'Accept-Language': language ? language : 'th',
+        'Accept-Language': language,
       },
       timeout: timeout,
     };
@@ -58,7 +59,7 @@ const Header = async (is_login = null, is_upload = null, timeout = 20000, requir
       headers: {
         'Content-Type': 'application/json',
         Accept: '*/*',
-        'Accept-Language': language ? language : 'th',
+        'Accept-Language': language,
       },
       timeout: timeout,
     };
