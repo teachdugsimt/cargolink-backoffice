40428cfaf4c3e7b52efac4ef566aa82764a2d2ee
tuntikorn <tuntikornwisuttikul@gmail.com>
2021-03-18 18:04:58 +0700

    [Update] close google

--- src/pages/test-map.tsx
+++ src/pages/test-map.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { GoogleMapWithSearch } from '../components';
+// import { GoogleMapWithSearch } from '../components';
 import { Card, CardHeader, CardBody } from '@paljs/ui/Card';
 import Alert from '../components/alert';
 import { defaultAlertSetting } from '../components/simple-data';
@@ -29,12 +29,12 @@ const TestMap = (props: any) => {
         <span>{t('selectLocation')}</span>
       </CardHeader>
       <CardBody style={{ flex: 1, width: '100%', height: '100%' }}>
-        <GoogleMapWithSearch
+        {/* <GoogleMapWithSearch
           google={new google.maps.DirectionsService()}
           center={{ lat: 13.736717, lng: 100.523186 }}
           height="400px"
           zoom={15}
-        />
+        /> */}
       </CardBody>
     </Card>
   );
