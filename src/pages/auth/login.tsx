41aae3609eeb590656eb2a0035090a7b8ddb062a
Katanyoo Ubalee <ubalee.k@gmail.com>
2021-06-20 21:21:25 +0700

    chore(auth/login): can request to login to dev host

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -71,7 +71,7 @@ const Login: React.FC<{ pageContext: { layout: string } }> = observer(({ pageCon
     setToggle(true);
     if (email && password) {
       loginStore.requestLogin({
-        loginId: email,
+        email: email,
         password: password,
         // userType: 0,
       });
