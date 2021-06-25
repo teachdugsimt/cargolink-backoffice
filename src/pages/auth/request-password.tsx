5cb9ef6ad4f516ac46b4939d28511fe766ee677a
Wallop Opasakhun <champbassist159@gmail.com>
2021-06-25 20:13:04 +0700

    fix(request password): fix request password email sent
    Change it from hardcoded text into translations text

--- src/pages/auth/request-password.tsx
+++ src/pages/auth/request-password.tsx
@@ -42,7 +42,7 @@ const RequestPassword = () => {
   const onSuccess = () => {
     Swal.fire({
       icon: 'success',
-      text: `Reset password link was sent to ${email}`,
+      text: t('sent', { email }),
     }).then(({ isConfirmed }) => {
       isConfirmed && navigate('/');
     });
