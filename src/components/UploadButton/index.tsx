e6e30c3ac0a2b290d32f01a6f694d274b5080da5
Wallop Opasakhun <champbassist159@gmail.com>
2021-06-28 17:50:32 +0700

    feat(user upload): finish bind user upload api to user upload page

--- src/components/UploadButton/index.tsx
+++ src/components/UploadButton/index.tsx
@@ -1,20 +1,27 @@
 import React, { useRef } from 'react';
 import Button from '@atlaskit/button/standard-button';
+import Spinner from '@atlaskit/spinner';
 import { useTranslation } from 'react-i18next';
 
 interface IProps {
   accept?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => any;
+  disabled?: boolean;
+  isLoading?: boolean;
 }
 const UploadButton = (props: IProps) => {
-  const { accept, onChange } = props;
+  const { accept, onChange, disabled, isLoading } = props;
   const { t } = useTranslation();
   const uploadInputRef = useRef<HTMLInputElement | null>(null);
   return (
     <>
-      <Button appearance="primary" onClick={() => uploadInputRef.current?.click()}>
-        {t('upload')}
-      </Button>
+      {isLoading ? (
+        <Spinner size="medium" />
+      ) : (
+        <Button appearance="primary" isDisabled={disabled} onClick={() => uploadInputRef.current?.click()}>
+          {t('upload')}
+        </Button>
+      )}
       <input type="file" accept={accept} onChange={onChange} ref={uploadInputRef} style={{ display: 'none' }} />
     </>
   );
