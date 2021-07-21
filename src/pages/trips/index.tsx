f698bc7fe3b8c8781edb0477c95f470125b0c795
Katanyoo Ubalee <ubalee.k@gmail.com>
2021-07-21 18:25:35 +0700

    fix(trips): temperary remove table

--- src/pages/trips/index.tsx
+++ src/pages/trips/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import Breadcrumbs, { BreadcrumbsItem } from '@atlaskit/breadcrumbs';
 import PageHeader from '@atlaskit/page-header';
+import DynamicTable from '@atlaskit/dynamic-table';
+
 import { useTranslation } from 'react-i18next';
+import styled from 'styled-components'
 
 function Trip() {
   const { t } = useTranslation();
@@ -14,8 +17,29 @@ function Trip() {
   return (
     <div>
       <PageHeader breadcrumbs={breadcrumbs}>{t('trip.management')}</PageHeader>
+
+      {/* <TableWrapper>
+        <DynamicTable
+          head={tableHeaders}
+          rows={extendRows(rows, onRowClick)}
+          page={page}
+          emptyView={<p>{t('noData')}</p>}
+          rowsPerPage={jobStore.data_jobs?.lengthPerPage || 10}
+          defaultPage={1}
+          loadingSpinnerSize="large"
+          isLoading={jobStore.loading}
+          onSort={onSort}
+          onSetPage={onPageChange}
+        />
+      </TableWrapper> */}
     </div>
   )
 }
 
 export default Trip
+
+const TableWrapper = styled.div`
+  margin-top: 10px;
+  text-align: center;
+  min-width: 600px;
+`;
